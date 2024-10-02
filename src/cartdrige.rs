cf220ebb0df70e5c238099b7e3ef8f8385f098d5
Siirko <alexander.yanovskyy@gmail.com>
2024-10-02 22:01:31 +0200

    added checksum and nintendo check

--- src/cartdrige.rs
+++ src/cartdrige.rs
@@ -1,10 +1,20 @@
 use std::fs::File;
 use std::io::Read;
 
+use log::info;
+
 #[repr(usize)]
-pub enum Address {
+enum Address {
     ROMSize = 0x148,
     CartridgeType = 0x147,
+    HeaderCheckSum = 0x14D,
+}
+
+#[warn(non_snake_case)]
+mod AddressRanges {
+    use std::ops::RangeInclusive;
+    pub const TITLE: RangeInclusive<u16> = 0x0134..=0x0143;
+    pub const CHECKSUM: RangeInclusive<u16> = 0x0134..=0x014C;
 }
 
 impl From<Address> for usize {
@@ -12,10 +22,48 @@ impl From<Address> for usize {
         a as usize
     }
 }
-
 pub trait Cartdrige: Send {
     fn read(&self, address: u16) -> u8;
     fn set(&mut self, address: u16, value: u8);
+
+    fn ensure_nintendo_logo(&self) {
+        const NINTENDO_LOGO: [u8; 48] = [
+            0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C,
+            0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6,
+            0xDD, 0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC,
+            0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E,
+        ];
+        for i in NINTENDO_LOGO.iter().enumerate() {
+            if self.read(0x0104 + i.0 as u16) != *i.1 {
+                panic!("Invalid Nintendo logo");
+            }
+        }
+    }
+
+    fn ensure_header_checksum(&self) {
+        let mut checksum: u8 = 0;
+        for i in AddressRanges::CHECKSUM {
+            checksum = checksum.wrapping_sub(self.read(i)).wrapping_sub(1);
+        }
+        if checksum != self.read(Address::HeaderCheckSum as u16) {
+            panic!(
+                "Invalid header checksum, expected {:#04x} but got {:#04x}",
+                checksum,
+                self.read(Address::HeaderCheckSum as u16)
+            );
+        }
+    }
+
+    fn get_title(&self) -> String {
+        let mut title = String::new();
+        for i in AddressRanges::TITLE {
+            match self.read(i) {
+                0 => break,
+                c => title.push(c as char),
+            }
+        }
+        title
+    }
 }
 
 pub struct RomOnly(pub Vec<u8>);
@@ -45,14 +93,22 @@ pub fn load(path: &str) -> Box<dyn Cartdrige> {
     }
 
     // Cartdrige type
-    match rom[Address::CartridgeType as usize] { 
+    let res: Box<dyn Cartdrige>;
+    match rom[Address::CartridgeType as usize] {
         0x00 => {
-            Box::new(RomOnly(rom))
-        },
+            res = Box::new(RomOnly(rom));
+        }
         _ => {
-            panic!("Unsupported cartdrige type: {:#04x}", rom[Address::CartridgeType as usize]);
+            panic!(
+                "Unsupported cartdrige type: {:#04x}",
+                rom[Address::CartridgeType as usize]
+            );
         }
     }
+    res.ensure_nintendo_logo();
+    res.ensure_header_checksum();
+    info!("ROM title: {}", res.get_title());
+    res
 }
 
 impl Cartdrige for RomOnly {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+mod cartdrige;
 mod cpu;
 mod register;
 mod window;
-mod cartdrige;
 
 use std::env;
 
+use log::info;
+
 pub fn main() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+    // set log level default to info
+    info!("starting up");
     let args: Vec<String> = env::args().collect();
     let rom_path = &args[1];
-
     // window::sdl2_example();
     let rom = cartdrige::load(rom_path);
 }
